544d54db37097e8d5887fab25268d872c1f5509d
James Hawk <jph94880@gmail.com>
2023-03-07 08:03:48 -0600

    Adds my file.md

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+
+ function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+ }
+
+ function appendCat(name) {
+    let copyOfCats = [...cats, name];
+    return copyOfCats;
+ }
+
+ function prependCat(name) {
+    let newCats = [name, ...cats];
+    return newCats;
+ }
+
+ function removeLastCat(name) {
+    let catsTwo = cats.slice(0, -1);
+    return catsTwo;
+ }
+
+ function removeFirstCat(name) {
+    let catsThree = cats.slice(1);
+    return catsThree;
+ }
+
